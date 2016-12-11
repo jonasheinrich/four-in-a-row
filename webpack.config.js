a9f72602c6d3f57bf7d04c6781420c7a86a5b10b
Jonas Heinrich <jonas.heinrich@qlearning.de>
2016-12-11 23:19:04 +0100

    Initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./entry.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+};
