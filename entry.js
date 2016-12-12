39f6fb24e63ec40ca97cf2709da49f78215a06dd
Jonas Heinrich <jonas.heinrich@qlearning.de>
2016-12-12 15:40:38 +0100

    Hello world from React

--- entry.js
+++ entry.js
@@ -1,1 +1,14 @@
-document.write("It works.");
+import React from 'react';
+import { render } from 'react-dom';
+
+class HelloReact extends React.Component {
+  render() {
+    return(
+      <div id='helloreact'>
+        Hello
+      </div>
+    )
+  }
+}
+
+render(<HelloReact />, document.getElementById('app'))
