91f70e2b1aa3564f36ad7907c05b6bc2df062efe
Jonas Heinrich <jonas.heinrich@qlearning.de>
2016-12-13 17:25:28 +0100

    Introduce app.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Board from './components/Board'
+import App from './app';
 import './app.css';
 
-render(<Board />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
